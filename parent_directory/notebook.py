65b916ef244ee0bf43302aee16e3e7c9929e05f4
815357393@qq.com <your@815357393@qq.com>
2016-02-22 19:28:25 +0800

    aa

---
+++ parent_directory/notebook.py
@@ -0,0 +1,53 @@
+import datetime
+
+last_id = 0
+
+
+class Note:
+    def __init__(self,memo,tags=''):
+        self.memo = memo
+        self.tags = tags
+        self.creat_date = datetime.date.today()
+        global last_id
+        last_id += 1
+        self.id = last_id
+
+    def match(self,filter):
+        return filter in self.memo or filter in self.tags
+
+
+class Notebook:
+
+    def __init__(self):
+        self.notes = []
+
+    def new_note(self,memo,tags=''):
+        self.notes.append(Note(memo,tags))
+
+    def _find_note(self,note_id):
+        for note in self.notes:
+            print(1)
+            if note.id == int(note_id.strip()):
+                
+                return note
+        return None
+
+    def modify_memo(self,note_id,memo):
+        note = self._find_note(note_id)
+        print(note)
+        if note:
+            note.memo = memo
+            return True
+        return False
+            
+
+    def modify_tags(self,note_id,tags):
+        tag=self._find_note(note_id)
+        if tag:
+            tag.tags = tags
+            return True
+        return False
+
+    def search(self,filter):
+        return [note for note in self.notes if note.match(filter)]
+        
